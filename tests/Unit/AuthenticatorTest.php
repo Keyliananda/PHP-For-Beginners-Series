e383c9ad71c51be0f04045cb4f4287a9051a541b
Keyliananda <info@piano-volz.de>
2024-07-18 21:49:30 +0200

    bevor ich's ALLES LÖSCH

---
+++ tests/Unit/AuthenticatorTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use Core\Authenticator;
+
+// weiter probieren
+//
+//test('it can resolve something out of the Authenticator', function () {
+//    $authenticator = new Authenticator();
+//
+//    $authenticator->attempt('geiler@gmx.de', 'gogo1234');
+//
+//    $result = $authenticator->resolve('foo');
+//
+//    expect($result)->toEqual('bar');
+//})
+//;
+//it('validates an email', function () {
+//    expect(Authenticator::email('foobar'))->toBeFalse();
+//});
